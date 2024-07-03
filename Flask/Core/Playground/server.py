ce45f0a674f07370ffbe9739f0fee82272645268
farouking <souissi.farouk@gmail.com>
2024-07-03 22:10:41 +0200

    03072024

---
+++ Flask/Core/Playground/server.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template  
+app = Flask(__name__)    
+#Level 01
+@app.route('/play')
+def play():
+    return render_template('index.html',times=3)
+@app.route('/play/<times>')
+def multiple(times):
+    return render_template('index.html',times=int(times))
+#Level 02 & 03
+@app.route('/play/<times>/<color>')
+def multiple_and_change(times,color):
+    return render_template('index.html',times=int(times),color='style=background-color:'+ color)
+
+
+if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
+    app.run(debug=True)    # Run the app in debug mode.s
